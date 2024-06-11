ddc46a2b599302560ea224b09a4e3b35aa74c958
Peter Seunghoon Yoo <yoopete1@mathlab.utsc.utoronto.ca>
2024-06-11 14:05:35 -0400

    hello

--- saveload.c
+++ saveload.c
@@ -2,13 +2,36 @@
 #include "point.h"
 
 ssize_t save_points(const char *pathname, const point *a, size_t n) {
-  return -1;
+ 	FILE *f = NULL;
+	f = fopen(pathname, "r+w");
+	if (f == NULL) return -1;
+	int num = fwrite(a, sizeof(point), n, f);
+	fclose(f);
+	return num;
 }
 
 ssize_t load_points(const char *pathname, point *a, size_t n) {
-  return -1;
+	FILE *f = NULL;
+	f = fopen(pathname, "r+w");
+	if (f == NULL) return -1;
+	int num = fread(a, sizeof(point), n, f);
+	fclose(f);
+	return num;
 }
 
 ssize_t scan_points(const char *pathname, point *a, size_t n) {
-  return -1;
+	FILE *f = NULL;
+	f = fopen(pathname, "r");
+	if (f == NULL) return -1;
+	int help;
+	int help2;
+	int num = 0;
+	while ( fscanf(f,"%d %d", &help, &help2 ) == 2){
+		//okay now we are reading things one at a time
+		point helper = {help, help2};
+		*a = helper;
+		a++;
+		num++;
+	}
+	return num;
 }
